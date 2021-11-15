d3ff26b4a9ed48fec6f47439d746aa99f524e2f9
pavel.sh <pavel.sh@gmail.com>
2021-11-15 16:27:08 +0300

    add weekly chest limit

--- contracts/contracts/ChestSale.sol
+++ contracts/contracts/ChestSale.sol
@@ -13,19 +13,26 @@ import "./Resource.sol";
 contract ChestSale is Ownable, ERC1155Holder {
     using SafeERC20 for IERC20;
 
+    struct UserStats {
+        uint256 weeksValue;
+        uint256 chestsBought;
+    }
+
     uint256 constant public TOTAL_CHESTS = 150000;
     uint256 constant public WEEK = 7 * 24 * 60 * 60;
     uint256 constant public TOTAL_WEEKS = 15;
     uint256 constant public CHESTS_PER_WEEK = TOTAL_CHESTS / TOTAL_WEEKS;
     uint256 constant public WEEK_BALANCE = CHESTS_PER_WEEK / 4;
+    uint256 constant public LIMIT_PER_USER = 250;
     uint256 public weekStart;
     uint256 public weeksLeft = TOTAL_WEEKS;
     uint256 public chestsLeft;
     uint256 public chestPricePhi = 10 ether;
     uint256 public chestPriceEth = .1 ether;
 
-    Resource private resource;
-    IERC20 private phi;
+    Resource private _resource;
+    IERC20 private _phi;
+    mapping (address => UserStats) private _userStats;
     uint256[4] public balances;
 
     event ChestsOpened(address indexed player, uint256[] tokenIds);
@@ -33,8 +40,8 @@ contract ChestSale is Ownable, ERC1155Holder {
     event ChestPriceEthUpdated(uint256 newValue);
 
     constructor(Resource resource_, IERC20 phi_) {
-        resource = resource_;
-        phi = phi_;
+        _resource = resource_;
+        _phi = phi_;
         _startWeek();
     }
 
@@ -50,9 +57,17 @@ contract ChestSale is Ownable, ERC1155Holder {
         require(count > 0 && count <= 500, "invalid count");
         require(chestsLeft >= count, "not enough available chests");
         require(msg.value == chestPriceEth * count, "incorrect value sent");
-        require(phi.balanceOf(msg.sender) >= chestPricePhi * count, "insufficient PHI balance");
+        require(_phi.balanceOf(msg.sender) >= chestPricePhi * count, "insufficient PHI balance");
 
-        phi.safeTransferFrom(msg.sender, address(this), chestPricePhi * count);
+        UserStats storage userStats = _userStats[msg.sender];
+        if (userStats.weeksValue != weeksLeft) {
+            userStats.chestsBought = 0;
+            userStats.weeksValue = weeksLeft;
+        }
+        require(userStats.chestsBought + count < LIMIT_PER_USER, "your weekly limit is exceeded");
+        userStats.chestsBought += count;
+
+        _phi.safeTransferFrom(msg.sender, address(this), chestPricePhi * count);
 
         if (block.timestamp - weekStart >= WEEK)
             _startWeek();
@@ -78,14 +93,14 @@ contract ChestSale is Ownable, ERC1155Holder {
             }
             tokenAmounts[tokenId] += 1;
         }
-        resource.safeBatchTransferFrom(address(this), msg.sender, tokenIds, tokenAmounts, "");
+        _resource.safeBatchTransferFrom(address(this), msg.sender, tokenIds, tokenAmounts, "");
         chestsLeft -= count;
         emit ChestsOpened(msg.sender, tokenIds);
     }
 
     function withdrawFees(address payable to) public onlyOwner {
         to.transfer(address(this).balance);
-        phi.safeTransferFrom(address(this), to, phi.balanceOf(address(this)));
+        _phi.safeTransferFrom(address(this), to, _phi.balanceOf(address(this)));
     }
 
     function updateChestPriceEth(uint256 newValue) public onlyOwner {
