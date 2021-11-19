858487b869ce1cbedcabe7c6ba466f2fa34334f4
pavel.sh <pavel.sh@gmail.com>
2021-11-19 15:44:40 +0300

    add resource to admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from app.models import MarketplaceListing
+from app.models import MarketplaceListing, Resource
 
 
 @admin.register(MarketplaceListing)
 class MarketplaceListingAdmin(admin.ModelAdmin):
     list_display = 'id', 'listing_id', 'resource', 'seller',
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = 'name', 'token_id', 'weight', 'ipfs_hash',
