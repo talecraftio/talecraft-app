3708ab7c9390793091a6129abe2a3df2b152e02b
pavel.sh <pavel.sh@gmail.com>
2021-12-21 12:24:22 +0300

    game update

--- contracts/scripts/mainnet_deploy_game.py
+++ contracts/scripts/mainnet_deploy_game.py
@@ -16,16 +16,16 @@ def main():
 
     games = addresses.get('games', {})
 
-    game1 = Game2.deploy(Resource[-1].address, PHI[-1].address, 10000000000000000000, 6, 50, {'from': deployer})
+    game1 = Game2.deploy(Resource[-1].address, PHI[-1].address, 0, 6, 50, {'from': deployer})
     games['0'] = game1.address
     snowtrace_publish(game1)
     game1.transferOwnership('0xd4AE6402155Ec508C6Ca7Dd833fd355c6eDd1c14', {'from': deployer})
 
-    game2 = Game2.deploy(Resource[-1].address, PHI[-1].address, 12000000000000000000, 51, 150, {'from': deployer})
+    game2 = Game2.deploy(Resource[-1].address, PHI[-1].address, 0, 51, 150, {'from': deployer})
     games['1'] = game2.address
     game2.transferOwnership('0xd4AE6402155Ec508C6Ca7Dd833fd355c6eDd1c14', {'from': deployer})
 
-    game3 = Game2.deploy(Resource[-1].address, PHI[-1].address, 15000000000000000000, 151, 1000, {'from': deployer})
+    game3 = Game2.deploy(Resource[-1].address, PHI[-1].address, 0, 151, 1000, {'from': deployer})
     games['2'] = game3.address
     game3.transferOwnership('0xd4AE6402155Ec508C6Ca7Dd833fd355c6eDd1c14', {'from': deployer})
 
